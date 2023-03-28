f681ef04b6a444ba36d3297eef2065bcb8599b43
Fahmi Muzakki <fahmirockman@gmail.com>
2023-03-28 16:02:34 +0700

    add cors v2

--- api/index.js
+++ api/index.js
@@ -4,6 +4,24 @@ const helmet = require('helmet');
 
 const app = express();
 app.use(express.static('public'));
+// Add headers before the routes are defined
+app.use((req, res, next) => {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '127.0.0.1:8000');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 app.use(cors({ origin: '*' }));
 app.use(helmet());
 require('./routes')(app, express);
