af344b2d0eb3e24c30b3d122e89a48478e6ecd2f
Fahmi Muzakki <fahmirockman@gmail.com>
2023-03-28 14:27:34 +0700

    add api endpoint

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,6 @@ const { getImage } = require('../controller/image.handler');
 
 module.exports = (app, express) => {
   app.get('/Images/profile/:img', getImage);
-  app.use('/profile', profile(express.Router(), validator));
+  app.use('/api/profile', profile(express.Router(), validator));
   return app;
 };
