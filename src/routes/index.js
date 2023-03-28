ad0e5763defd59b7353ddf3e3927d8acb1bd9d3a
Fahmi Muzakki <fahmirockman@gmail.com>
2023-03-28 14:36:15 +0700

    add vercel

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,6 @@ const { getImage } = require('../controller/image.handler');
 module.exports = (app, express) => {
   app.get('/', (req, res) => { res.json('Hello World'); });
   app.get('/Images/profile/:img', getImage);
-  app.use('/api/profile', profile(express.Router(), validator));
+  app.use('/profile', profile(express.Router(), validator));
   return app;
 };
